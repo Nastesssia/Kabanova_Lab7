d04b357df8f299b8bb204ca88208193c53b30a50
alexa <alexa@DESKTOP-RV4BEBB>
2022-06-08 15:53:28 +0200

    Добавьте файлы проекта.

---
+++ Kabanova_Lab7/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kabanova_Lab7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<double> list;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            int n = random.Next(100);
+            list = new List<double>();
+            string str = "";
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(random.NextDouble() * 100);
+                str += $"{list[i]:F2} ";
+            }
+            Result.Text = str;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            for(int i = 0; i < list.Count; i++)
+            {
+                if (list[0] > 0)
+                {
+                    list.Insert(i, list[0]);
+
+                }
+            }
+            string str = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                str += $"{list[i]:F2} ";
+            }
+            Result.Text = str;
+
+            //double S = 0;
+            //foreach (double i in list)
+            //{
+            //    S += i;
+            //}
+            //double avg = S / list.Count;
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    if (list[i] > avg)
+            //    {
+            //        list.Insert(i, 11);
+            //        i++;
+            //    }
+
+            //}
+            //string str = "";
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    str += $"{list[i]:F2} ";
+            //}
+            //Result.Text = str;
+        }
+    }
+}
